f903791de3beba166eacd743a9259bcfd446c608
soumya singh <as1011930@gmail.com>
2022-08-31 22:39:39 +0530

    auth3

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,10 +1,26 @@
 const jwt= require('jsonwebtoken')
 const middleware=require("../middleware/auth.js")
-const authenticate= async function(req, res){
-    let userId=req.params.userId
+const authenticate= async function(req, res,next){
     let token=req.headers["x-auth-token"]
     if(!token) return res.send({status:false,msg:"token is required"})
+     let decodeToken= jwt.verify(token,'SAmSIM-Sr5slt178')
+     req.decodeToken=decodeToken
+     req.Sameer="ratagsdaseavajaok"
     next()
 }
+  
+const authorization=async function(req,res,next){
+    let userId=req.params.userId
+    let decodeTokenId=req.decodeToken.userId
+    if(userId==decodeTokenId){
+        next()
+    }
+    return res.send({status:false,msg:"person is not authorised"})
+    
+} 
+
+
+
 
 module.exports.authenticate=authenticate
+module.exports.authorization=authorization
--- src/routes/route.js
+++ src/routes/route.js
@@ -5,16 +5,16 @@ const middleware= require("../middleware/auth")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
-})
+}),
 
 router.post("/Users", userController.createUser  )
 
 router.post("/login", userController.loginUser)
 
 // //The userId is sent by front end
- router.get("/users/:userId",middleware.auth,userController.getUser)
+ router.get("/users/:userId",middleware.authenticate,middleware.authorization,userController.getUser)
 
- router.put("/users/:userId",middleware.auth,userController.updateUser)
- router.delete("/users/:userId",middleware.auth,userController.deletetUser)
+ router.put("/users/:userId",middleware.authenticate,middleware.authorization,userController.updateUser)
+ router.delete("/users/:userId",middleware.authenticate,middleware.authorization,userController.deletetUser)
 
 module.exports = router;
